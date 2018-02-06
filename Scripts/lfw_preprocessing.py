18de5e5a71bf4b4291bca3ab2be2759df5009026
Somnath Sarkar <somnath.k.sarkar@gmail.com>
2018-02-06 12:14:30 +0530

    Added variable size preprocessing script for LFW

--- Scripts/lfw_preprocessing.py
+++ Scripts/lfw_preprocessing.py
@@ -6,13 +6,15 @@ import glob
 img_list = glob.glob("../Data/lfw/**/*.jpg")
 img_data = []
 
-for img_file in img_list:
+for i in range(len(img_list)):
+    img_file = img_list[i]
     img = cv2.imread(img_file)
     gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    for i in range(3):
-        gray_img = cv2.pyrDown(gray_img)
+    gray_img = cv2.resize(gray_img, (64,64), interpolation=cv2.INTER_AREA)
     gray_img_list = [img_file]+np.ravel(gray_img).tolist()
     img_data.append(gray_img_list)
+    if (i%100)==0:
+        print("Converted {}/{}".format(i+1,len(img_list)))
 
 img_df = pd.DataFrame(img_data)
-img_df.to_csv("../Data/lfw_grayscale_32.csv",header=False,index=False)
+img_df.to_csv("../Data/lfw_grayscale_64.csv",header=False,index=False)
